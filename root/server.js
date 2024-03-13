a288b8ce622af0f58ad179a8e4b7d7e59cb15429
rjzin <bigriann18@gmail.com>
2024-03-13 20:36:21 -0300

    update day 13/03

---
+++ root/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs'); // Importando o módulo 'fs'
+const app = express();
+const port = 3030;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/poke', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index_poke.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando: http://localhost:${port}`);
+});
